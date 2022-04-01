c47cb17909f27f5e2629e922ed5500ce4a9f8b99
Alexuva <alejandrohernandezrosa@gmail.com>
2022-04-01 21:49:00 +0200

    final revision

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,40 @@
 let questions = document.querySelectorAll('.q');
+let qId = '';
+
+function close(){
+    questions.forEach( question => {
+        let answer = question.querySelector('.a');
+        let ask =  question.querySelector('.ask');
+        if(question.id != qId && answer.classList.contains('active')){
+            answer.classList.remove('active');
+            ask.classList.remove('bold');
+        }
+    })
+}
 
 questions.forEach(question => {
-    question.addEventListener('click', function(){
+    
+    question.addEventListener('click', (e) => {
+
+        e.preventDefault();
         
-        this.classList.toggle('.active');
-
-        let answer = this.querySelector('.a');
-
-        if(answer.style.display == 'none'){
-            answer.style.display = 'block';
+        let answer = question.querySelector('.a');
+        let ask =  question.querySelector('.ask');
+        
+        if(answer.classList.contains('active')){
+            answer.classList.remove('active');
+            ask.classList.remove('bold');
         }else{
-            answer.style.display = 'none';
+            ask.classList.add('bold');
+            answer.classList.add('active');
+            qId = question.id;
+            close();
+            console.log(qId);
         }
-            
-    });
-});
+    })
+})
+
+    //Comprobar si hay alguna respuesta abierta
+    //Cerrar la respuesta abierta
+    //Comprobar que respuesta se ha clickeado
+    //Abrir respuesta clickeada
